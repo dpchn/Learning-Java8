c1141e43adc36f49d5b7f4334af63a5446170d1f
asamavedula <asamavedula@care.com>
2017-07-10 12:26:05 +0530

    diamond problem and method referecne examples

--- MethodReference/src/A.java
+++ MethodReference/src/A.java
@@ -3,8 +3,31 @@ import java.util.function.*;
 import java.util.*;
 public class A {
 
+	public String instanceName;
+	
+	A(String instanceName)
+	{
+		System.out.println("Creating instance "+instanceName);
+		this.instanceName = instanceName;
+	}
+	
+	public void print(String something)
+	{
+		System.out.println("testing instance methnd call "+instanceName+"  "+something);
+	}
+	
+	public static void printStatic(String something)
+	{
+		System.out.println("testing instance methnd call "+something);
+	}
+	
+	public void printInstance(String something)
+	{
+		System.out.println("testing instance methnd call "+something);
+	}
+	
 	public static void main(String[] args) {
-		Consumer<String> printer = x-> System.out.println(x);
+		/*Consumer<String> printer = x-> System.out.println(x);
 		Consumer<String> printString =  System.out::println;
 		
 		
@@ -20,13 +43,50 @@ public class A {
 				System.out.println(str);
 			}
 		};
-		i.show(" Hey");
+		i.show(" Hey");*/
 	
 		/*
 		 * Above Code is written in  below line
 		 */
-		iterfaceTest i1 = System.out::println;
-		i1.show(" World");
+		
+		
+		A a1 = new A("instance 1");
+		
+		A a2 = new A("instance 2");
+		
+		// call to method of an instance
+		//iterfaceTest i3 = x -> { a1.print(x) };
+		iterfaceTest i1 = a1::print;
+		i1.show("i1 show method ");
+		
+		//iterfaceTest i3 = x -> { a1.print(x); };
+		iterfaceTest i2 = a2::print;
+		i2.show("i2 show method ");
+		
+		
+		// call to a static method
+		//iterfaceTest i3 = x -> { A.printStatic(x); };
+		iterfaceTest i3 = A::printStatic;
+		i3.show("i3 show method");
+		
+		
+		// call to constructor
+		//iterfaceTest i3 = x -> { new A(x); };
+		
+		iterfaceTest i4 = A::new; 
+		i4.show("i4 show method");
+		
+		//iterfaceTest i3 = x -> { x.toUpperCase() };
+		iterfaceTest i5 = String::toUpperCase; 
+		i5.show("i4 show method");
+		
+		
+		
+		
+		
+		
+		
+		//i1.show(" World");
 	}
 }
 
--- tut_09_DiamondProblem/src/A.java
+++ tut_09_DiamondProblem/src/A.java
@@ -1,50 +1,33 @@
+interface A {
 
-public class A {
-	public void name() {
-		System.out.println("Hello");
+	default void test() {
+		System.out.println("In interface A");
 	}
 }
 
-class B extends A{
-	
-	
-	public void name() {
-		System.out.println("Class B");
+interface B extends A {
+	default void test() {
+		System.out.println("In interface B");
 	}
 }
 
-class C extends A{
-	public void name() {
-		System.out.println("Class C");
+interface C extends A {
+	default void test() {
+		System.out.println("In interface B");
 	}
 }
 
-
-class D extends B,C {// Java confuse of which class function should use
-	public void name() {
-		System.out.println("Class B");
+interface D extends B, C {
+	default void test() {
+		System.out.println("In interface D");
 	}
 }
 
+class MainClass {
+	public static void main(String[] args) {
 
-
-interface i1{
-	public abstract void foo();
-}
-
-
-interface i2 extends i1{
-	
-	public abstract void foo();
-}
-
-interface i3 extends i1{
-	public abstract void foo();
-}
-
-interface i4 extends i2, i3{
-	public abstract void foo(){
-		
+		D d = new D() {
+		};
+		d.test();
 	}
 }
-
